3e940d2a1f92ea9aa465ae5c9c86c700691d9989
Petrovic Marko <marko.petrovic@wolterskluwer.com>
2026-04-01 12:32:24 +0200

    New

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,17 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18055944609"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-18055944609');`}
+      </Script>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         {children}
       </body>
