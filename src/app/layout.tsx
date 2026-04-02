380f095ffb08ada47685b9cca1395a074ebe8351
Petrovic Marko <marko.petrovic@wolterskluwer.com>
2026-04-02 14:06:24 +0200

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '871481225940898');
+fbq('init', '867462693034491');
 fbq('track', 'PageView');`}
       </Script>
       <body
@@ -62,7 +62,7 @@ fbq('track', 'PageView');`}
             height="1"
             width="1"
             className="hidden"
-            src="https://www.facebook.com/tr?id=871481225940898&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=867462693034491&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
